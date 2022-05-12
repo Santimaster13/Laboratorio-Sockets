6385a548a577d2712c93b27f99bbc737d93bd964
Santi Mercado <Santi Mercado@Black-Knight>
2022-05-11 19:15:51 -0500

    Final Test

---
+++ src/lab03/Cliente.java
@@ -0,0 +1,306 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab03;
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Santi Mercado
+ */
+public class Cliente {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private ArrayList<Integer> vector;
+    private ArrayList<Integer> resultado;
+    
+    public ArrayList<Integer> GetResultado(){
+        return this.vector;
+    }
+
+    public void Connect_to_Server(String ip, int port){
+        try {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (Exception e) {
+            
+        }
+    }
+    
+    public void Operacion(){
+        try {
+            System.out.println("Connection Succesful");
+            vector = new ArrayList<Integer>();
+            String tam = in.readLine();
+            int tamaño = Integer.parseInt(tam);
+            for (int i=0; i<tamaño; i++){
+                    vector.add(Integer.parseInt(in.readLine()));
+            }
+            int opcion = Integer.parseInt(in.readLine());
+            int pivote = Integer.parseInt(in.readLine());
+            long startTime = System.nanoTime();      
+            if(opcion == 1){
+                Mergesort(0, vector.size()-1);
+            }
+            if (opcion == 2){
+                Heapsort();
+            } 
+            if(opcion == 3){
+                if (pivote == 1){
+                  QuicksortL(vector.size()-1, 0);   
+                } else {
+                   QuicksortR(vector.size()-1, 0);  
+                }
+            }
+            long estimatedTime = System.nanoTime() - startTime;
+            sendMessage(".");
+            sendMessage(Long.toString(estimatedTime));
+            EnviarV(vector);
+            stop();
+        } catch (Exception e){
+            System.out.println("Connection Failure");
+        }
+        
+    }
+    
+    public void Mergesort(int izq, int der){
+        if (izq < der){
+            int centro=(izq+der)/2;
+            Mergesort(izq, centro);
+            Mergesort(centro+1, der);                                                                                
+            Merge(izq, centro, der);                                                                                 
+    }
+    }
+    
+    public void Merge(int izq, int centro, int der){
+       int i, j, k;
+   int [] vec2 = new int[vector.size()]; 
+   for (i=izq; i<=der; i++){
+       vec2[i] = vector.get(i);
+   }                                           
+
+   i=izq; j=centro+1; k=izq;
+     
+   while (i<=centro && j<=der){
+      if (vec2[i]<=vec2[j]){
+          if (k != i){
+              sendMessage("Confirmed");
+         sendMessage("Replacing the value at position " + k + " to " + vec2[i]);
+          sendMessage("Succesful");
+          sendMessage("");
+          vector.set(k, vec2[i]);
+          }
+             
+             i++;
+             k++;
+          } else{
+          if (k != j){
+             vector.set(k, vec2[j]);
+          sendMessage("Confirmed");
+          sendMessage("Replacing the value at position " + k + " to " + vec2[j]);
+          sendMessage("Succesful");
+          sendMessage(""); 
+          }
+          
+          k++;
+          j++; 
+          }
+   }                                    
+              
+   while (i<=centro) {
+       if (k != i){
+               vector.set(k, vec2[i]);
+      sendMessage("Confirmed");
+          sendMessage("Replacing the value at position " + k + " to " + vec2[i]);
+          sendMessage("Succesful");
+          sendMessage("");  
+       }
+      k++;
+      i++;
+   }          
+        
+    }
+    
+    public void Heapsort(){
+        for (int i = (vector.size()/2) - 1; i >= 0; i--) {
+        Heapify(vector.size(), i);
+    }
+    
+    for (int i = vector.size() - 1; i >= 0; i--) {
+        int temp = vector.get(0);
+        vector.set(0, vector.get(i));
+        vector.set(i, temp);
+        Heapify(i, 0);
+    }
+       
+    }
+    
+    public void Heapify(int tam, int a){
+      int izq = (2 * a)+ 1;
+    int der = (2 * a) + 2;
+    int mayor = a;
+    if (izq < tam && vector.get(izq) > vector.get(mayor)) {
+        mayor = izq;
+    }
+    if (der < tam && vector.get(der) > vector.get(mayor)) {
+        mayor = der;
+    }
+    if (mayor != a) {
+        int temp = vector.get(a);
+        vector.set(a, vector.get(mayor));
+        vector.set(mayor, temp);
+        sendMessage("Confirmed");
+         sendMessage("Swapping the value at positions " + a + " and " + mayor);
+          sendMessage("Succesful");
+          sendMessage("");
+        Heapify(tam, mayor);
+    }  
+    }
+    
+    public void QuicksortL(int der, int izq){
+        int pivote = vector.get(izq);
+        int i=izq;    
+        int j=der;         
+ 
+  while(i < j){
+        while(vector.get(i) <= pivote && i < j){ //<=
+         i++;
+     } 
+     while(vector.get(j) > pivote){
+         j--;
+     }     
+     
+             
+     if (i < j) { 
+         int temp = vector.get(i);
+         vector.set(i, vector.get(j));
+         vector.set(j, temp);
+         sendMessage("Confirmed");
+         sendMessage("Swapping the value at positions " + i + " and " + j);
+          sendMessage("Succesful");
+          sendMessage("");
+     }
+   }
+  
+  if (izq != j){
+     vector.set(izq, vector.get(j));
+   vector.set(j, pivote); 
+      sendMessage("Confirmed");
+         sendMessage("Swapping the value at position " + izq + " and " +  j);
+          sendMessage("Succesful");
+          sendMessage(""); 
+  }
+     
+             if(izq < j-1){
+     QuicksortL(j-1, izq);      
+   }
+            
+   if(j+1 < der){
+     QuicksortL(der, j+1);   
+   }  
+    }
+    
+    public void QuicksortR(int der, int izq){
+        int pivote = vector.get(der);
+        int i=izq;    
+        int j=der;         
+ 
+  while(i < j){
+        while(vector.get(i) < pivote && i < j){ //<=
+         i++;
+     } 
+     while(vector.get(j) >= pivote && i < j){
+         j--;
+     }     
+     
+             
+     if (i < j) { 
+         int temp = vector.get(i);
+         vector.set(i, vector.get(j));
+         vector.set(j, temp);
+         sendMessage("Confirmed");
+         sendMessage("Swapping the value at positions " + i + " and " + j);
+          sendMessage("Succesful");
+          sendMessage("");
+     }
+   }
+  if (der != i){
+     vector.set(der, vector.get(i));
+   vector.set(i, pivote); 
+      sendMessage("Confirmed");
+         sendMessage("Swapping the value at position " + der + " and " +  j);
+          sendMessage("Succesful");
+          sendMessage("");
+  }
+     
+             if(izq < j-1){
+     QuicksortR(j-1, izq);      
+   }
+            
+   if(j+1 < der){
+     QuicksortR(der, j+1);   
+   }  
+    }
+
+    public void sendMessage(String msg){
+        try {
+            out.println(msg);
+        } catch (Exception e) {
+            System.out.println("error de impresion");
+        }
+    } 
+    
+    public void ImprimirV(ArrayList<Integer> vector){
+        for (int i=0; i<vector.size()-1; i++){
+            System.out.print(vector.get(i)+ " --> ");
+        }
+        int x = vector.size()-1;
+        System.out.print(vector.get(x));
+        System.out.println("");
+    }
+    
+    public void EnviarV(ArrayList<Integer> vector){
+        for (int i=0; i<vector.size(); i++){
+            out.println(vector.get(i));
+        }
+    }
+    
+    public void RecibirV(ArrayList<Integer> vector){
+       for (int i=0; i<vector.size(); i++){
+           try {
+               vector.set(i, Integer.parseInt(in.readLine()));
+           } catch (IOException ex) {
+               Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        } 
+    }
+
+    public void stop(){
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args){
+        Cliente c = new Cliente();
+        String ip;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the server's IP address");
+        ip = sc.nextLine();
+        c.Connect_to_Server(ip, 5555);
+        System.out.println("Establishing connection...");
+        c.Operacion();
+    }
+}
---
+++ src/lab03/Servidor.java
@@ -0,0 +1,250 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab03;
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Santi Mercado
+ */
+public class Servidor {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    
+    public static boolean StrNum(String str) {
+    if (str == null) {
+        return false;
+    }
+    try {
+        int t = Integer.parseInt(str);
+    } catch (NumberFormatException nfe) {
+        return false;
+    }
+    return true;
+}
+    public void CorregirVector(ArrayList<Integer> vector){
+        boolean keep = false;
+        do {
+            keep = false;
+          for (int i=0; i < vector.size(); i++){
+            for (int j=i+1; j < vector.size(); j++){
+                if (vector.get(i) == vector.get(j)){
+                    Random r = new Random();
+                    vector.set(j, r.nextInt(99999));
+                    keep = true;
+                }
+            }
+        }  
+        } while(keep == true);
+        
+    }
+    
+    public void start(int port){
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Welcome, I'm the Great Sage!");
+            System.out.println("Notice:");
+            System.out.println("In order to start, please log in as Client in another PC");
+            System.out.println("Please enter the following IP address in the client: ");
+            String ip = InetAddress.getLocalHost().toString();
+            boolean find = false;
+            int pos = 0;
+            while (find == false && pos < ip.length()){
+                if (ip.substring(pos, pos+1).equals("/")){
+                   find = true; 
+                } 
+                 pos++;
+            }
+            System.out.println(ip.substring(pos));
+            System.out.println("Establishing connection...");
+            clientSocket = serverSocket.accept();
+            System.out.println("Succesful");
+            System.out.println("");
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Scanner sc = new Scanner(System.in);
+            boolean correct = false;
+            System.out.println("Notice:");
+            System.out.println("Please enter the array's size");
+            String input;
+            while (correct==false){
+            input = sc.nextLine();
+                System.out.println("Genearting array...");
+              if (StrNum(input) == true){
+                int tam = Integer.parseInt(input);
+                if (tam > 1){
+                ArrayList<Integer> vec = new ArrayList<Integer>();
+                for (int i = 0; i < tam; i++) {
+                   Random r = new Random();
+                   vec.add(r.nextInt(99999)); 
+                }
+                CorregirVector(vec);
+                correct = true;
+                System.out.println("Succesful");
+                System.out.println("");
+                System.out.println("Report: Array will be ordered from lowest to highest");
+                System.out.println("Notice:");
+                  System.out.println("Please select the ordering method");
+                System.out.println("1. Megesort");
+                System.out.println("2. Heapsort");
+                System.out.println("3. Quicksort");
+                System.out.println("4. Salir");
+                boolean correct2 = false;
+                while(correct2 == false){
+                  input = sc.nextLine();
+                    System.out.println("Selecting option...");
+                if (input.equals("1") || input.equals("2") || input.equals("3")){
+                    correct2 = true;
+                    System.out.println("Succesful");
+                    System.out.println("");
+                    int opcion = Integer.parseInt(input);
+                    int pivote = 0;
+                    
+                    if (input.equals("3")){
+                        boolean correct3 = false;
+                        System.out.println("Report: This algorithm requires a pivot");
+                        System.out.println("Notice:");
+                        System.out.println("Please select a pivot");
+                        System.out.println("1. Leftmost value");
+                        System.out.println("2. Rightmost value");
+                        while(correct3 == false){
+                            input = sc.nextLine();
+                            correct3 = true;
+                            System.out.println("Selecting pivot...");
+                            if (input.equals("1") || input.equals("2")){
+                                pivote = Integer.parseInt(input);
+                                System.out.println("Succesful");
+                            } else {
+                                System.out.println("Failed");
+                      System.out.println("Notice:");  
+                      System.out.println("Please enter a valid option");
+                      System.out.println("");
+                            }
+                        }
+                    }              
+                    System.out.println("Notice: ");
+                    System.out.println("All data is going to be delivered to the Client, who will order the array and return it ordered.");
+                    System.out.println("");
+                    out.println(tam);
+                    EnviarV(vec);
+                    out.println(opcion);
+                    out.println(pivote);
+                    System.out.println("Succesful");
+                    System.out.println("");
+                    System.out.println("Notice:");
+                    System.out.println("The original array is as follows");
+                    ImprimirV(vec);
+                    System.out.println("Starting the sort...");
+                    System.out.println("");
+                    boolean ordered = false;
+                    while(ordered == false && (input = in.readLine()) != null){
+                if(".".equals(input)){
+                    System.out.println("Confirmed");
+                    System.out.println("Array succesfully sorted");
+                    System.out.println("Elapsed time: "+ in.readLine()+ " nanoseconds");
+                    System.out.println("The sorted array is as follows: ");
+                    RecibirV(vec);
+                    ImprimirV(vec);
+                    System.out.println("");
+                    System.out.println("Thats all folk!");
+                    ordered = true;
+                    //stop();
+                } else {
+                System.out.println(input);
+                }
+            }
+                } else {
+                    if(input.equals("4")){
+                        System.out.println("Succesful");
+                        System.out.println("Thats all folk!");
+                        System.exit(0);
+                    } else {
+                      System.out.println("Failed");
+                      System.out.println("Notice:");  
+                      System.out.println("Please enter a valid option");
+                      System.out.println("");
+                    }
+                    
+                } 
+                
+                }
+                
+              } else {
+                      System.out.println("Failed");
+                      System.out.println("Notice:");  
+                      System.out.println("Please enter an array size bigger than 1");
+                      System.out.println("");
+              }  
+                
+            } else {
+                     System.out.println("Failed");
+                      System.out.println("Notice:");  
+                      System.out.println("Please enter a valid size");
+                      System.out.println("");
+            }  
+            }
+            
+            String cliente_input;
+            while((cliente_input = in.readLine()) != null){
+               out.println(cliente_input);
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    public void ImprimirV(ArrayList<Integer> vector){
+        for (int i=0; i<vector.size()-1; i++){
+            System.out.print(vector.get(i)+ " --> ");
+        }
+        int x = vector.size()-1;
+        System.out.print(vector.get(x));
+        System.out.println("");
+    }
+    
+    public void EnviarV(ArrayList<Integer> vector){
+        for (int i=0; i<vector.size(); i++){
+            out.println(vector.get(i));
+        }
+    }
+    
+    public void RecibirV(ArrayList<Integer> vector){
+       for (int i=0; i<vector.size(); i++){
+           try {
+               vector.set(i, Integer.parseInt(in.readLine()));
+           } catch (IOException ex) {
+               Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        } 
+    }
+
+    public void stop(){
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+        Servidor l = new Servidor();
+        l.start(5555);
+    }
+    
+}
